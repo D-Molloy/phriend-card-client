17adbfe91874fd592e803a5cecd49847677de02b
Denis Molloy <denis.molloy@gmail.com>
2020-06-15 21:05:45 -0700

    setup grid on overview

--- src/components/UserOverview.vue
+++ src/components/UserOverview.vue
@@ -1,27 +1,42 @@
 <template>
-  <div v-if="user.showScoreAverage">
-    <h1>{{ user.username }}'s PhriendScore</h1>
-    <p>Avg. Show Score: {{ user.showScoreAverage.toFixed(3) }}</p>
-    <p>Shows Attended: {{ user.shows.length }}</p>
-    <hr />
-    <p>Total Venues: {{ user.venueSummary.length }}</p>
-    <p>
-      Favorite venue: {{ user.venueSummary[0].venue }} ({{
-        user.venueSummary[0].shows.length
-      }}
-      show{{ user.venueSummary[0].shows.length > 1 ? "s" : "" }})
-    </p>
-    <p>Top Venue: {{ topShow.venue }} ({{ topShow.venueRating.toFixed(3) }})</p>
-    <hr />
-    <p>Songs heard: {{ user.totalSongsHeard }}</p>
-    <p>Frequent Songs:</p>
-    <p v-for="song in user.songFrequency.slice(0, 3)" :key="song[0]">
-      {{ song[0] }} x {{ song[1] }}
-    </p>
-    <hr />
-    <scores-by-day :days="user.avgShowScoreByDay" />
-    <scores-by-year :years="user.avgShowScoreByYear" />
-    <div>
+  <div v-if="user.showScoreAverage" class="grid">
+    <div class="grid_score item_bg">
+      <h1>
+        {{ user.username }}'s PhriendScore:
+        {{ user.showScoreAverage.toFixed(3) }}
+      </h1>
+    </div>
+    <div class="grid_total_shows item_bg">
+      <p>Shows Seen: {{ user.shows.length }}</p>
+    </div>
+    <div class="grid_total_songs item_bg">
+      <p>Songs heard: {{ user.totalSongsHeard }}</p>
+    </div>
+    <div class="grid_venue_summary item_bg">
+      <p>Total Venues: {{ user.venueSummary.length }}</p>
+      <p>
+        Favorite venue: {{ user.venueSummary[0].venue }} ({{
+          user.venueSummary[0].shows.length
+        }}
+        show{{ user.venueSummary[0].shows.length > 1 ? "s" : "" }})
+      </p>
+      <p>
+        Best Venue: {{ topShow.venue }} ({{ topShow.venueRating.toFixed(3) }})
+      </p>
+    </div>
+    <div class="grid_frequent item_bg">
+      <p>Frequent Songs:</p>
+      <p v-for="song in user.songFrequency.slice(0, 3)" :key="song[0]">
+        {{ song[0] }} x {{ song[1] }}
+      </p>
+    </div>
+    <div class="grid_days item_bg">
+      <scores-by-day :days="user.avgShowScoreByDay" />
+    </div>
+    <div class="grid_years item_bg">
+      <scores-by-year :years="user.avgShowScoreByYear" />
+    </div>
+    <div class="grid_best item_bg">
       <h1>Best Show</h1>
       <h3>
         {{ user.showBest.venue }}, {{ user.showBest.location }} ------
@@ -44,7 +59,7 @@
         </p>
       </div>
     </div>
-    <div>
+    <div class="grid_worst item_bg">
       <h1>Worst Show</h1>
       <h3>
         {{ user.showWorst.venue }}, {{ user.showWorst.location }} ------
@@ -69,6 +84,52 @@
     </div>
   </div>
 </template>
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  /* grid-template-rows: repeat(5, 1fr); */
+  /* grid-template-rows: minmax(min-content, max-content); */
+  grid-template-rows: auto;
+  grid-auto-rows: auto;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  padding: 20px;
+}
+
+.grid_score {
+  grid-area: 1 / 1 / 2 / 3;
+}
+.grid_total_shows {
+  grid-area: 1 / 3 / 2 / 4;
+}
+.grid_total_songs {
+  grid-area: 1 / 4 / 2 / 5;
+}
+.grid_venue_summary {
+  grid-area: 2 / 1 / 3 / 2;
+}
+.grid_frequent {
+  grid-area: 2 / 2 / 3 / 3;
+}
+.grid_days {
+  grid-area: 2 / 3 / 3 / 4;
+}
+.grid_years {
+  grid-area: 2 / 4 / 3 / 5;
+}
+.grid_best {
+  grid-area: 3 / 1 / 5 / 3;
+}
+.grid_worst {
+  grid-area: 3 / 3 / 5 / 5;
+}
+.item_bg {
+  background-color: white;
+  border: 2px solid var(--red);
+  border-radius: 9px;
+}
+</style>
 
 <script>
 import ScoresByYear from "@/components/ScoresByYear.vue";
